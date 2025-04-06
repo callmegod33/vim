873b090a82cf6025e72545c1ede2bd1f163d214b
hectorbananabread <btrglm@proton.me>
2025-04-06 03:28:25 -0600

    qol plugins added

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,23 +1,16 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({
-			variant = "moon",
-			disable_background = true,
-			styles = {
-				italic = false,
-			},
-		})
-
-		ColorMyPencils()
-	end,
+	{
+		"zenbones-theme/zenbones.nvim",
+		-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+		-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+		-- In Vim, compat mode is turned on as Lush only works in Neovim.
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
+		-- you can set set configuration options here
+		config = function()
+			vim.g.zenbones_darken_comments = 45
+			vim.cmd.colorscheme("kanagawabones")
+		end,
+	},
 }
