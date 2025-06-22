3f9a902283fd040b146c06425dfd0bc761dfd3c3
callmegod33 <btrglm@proton.me>
2025-06-22 07:16:47 -0600

    up

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,12 +25,12 @@ vim.opt.timeoutlen = 300
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 8
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
 vim.opt.expandtab = true
 vim.opt.smartindent = true
--- vim.opt.autoindent = true
+vim.opt.autoindent = true
 
 vim.opt.inccommand = "split"
 
